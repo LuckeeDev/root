e464e148654e877907e13e1b3bf4de09a56dd8b6
Luca Zoppetti <luckeedev@gmail.com>
2024-01-17 11:02:11 +0100

    fix question 1 180115

--- exams/second_year/180115/macro1.C
+++ exams/second_year/180115/macro1.C
@@ -4,22 +4,24 @@
 void macro1() {
   gRandom->SetSeed();
 
-  TH1F* histo1 = new TH1F{"histo1", "Histogram 1", 1000, 0., 5.};
-  TH1F* histo2 = new TH1F{"histo2", "Histogram 2", 1000, 0., 5.};
+  TH1F* h_gen = new TH1F{"h_gen", "Histogram 1", 1000, 0., 5.};
+  h_gen->Sumw2();
+  TH1F* h_acc = new TH1F{"h_acc", "Histogram 2", 1000, 0., 5.};
+  h_acc->Sumw2();
 
   for (int j{}; j < 1e7; ++j) {
     auto x = gRandom->Exp(1);
-    histo1->Fill(x);
+    h_gen->Fill(x);
 
     auto y = gRandom->Uniform(0, 1);
     if (y < x / 5) {
-      histo2->Fill(x);
+      h_acc->Fill(x);
     }
   }
 
-  TH1F* histo_eff = new TH1F(*histo2);
-  histo_eff->SetName("histo_eff");
-  histo_eff->SetTitle("Efficiency");
-  histo_eff->Divide(histo2, histo1, 1, 1, "B");
-  histo_eff->Draw("E");
+  TH1F* h_eff = new TH1F(*h_acc);
+  h_eff->SetName("h_eff");
+  h_eff->SetTitle("Efficiency");
+  h_eff->Divide(h_acc, h_gen, 1, 1, "B");
+  h_eff->Draw("E");
 }
